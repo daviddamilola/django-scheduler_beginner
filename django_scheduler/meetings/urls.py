c67d8a7c9677478f8186aaba19f0bfed12fbf0a7
david_oluwasusi <daviddamilola20@gmail.com>
2021-01-02 17:52:06 +0100

    completed django beginner

---
+++ django_scheduler/meetings/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('<int:id>', views.detail, name='detail'),
+    path('room/<int:id>', views.room, name="room"),
+    path('new', views.new, name='new')
+]
